21442c156d30a781a0e81f51accb6f344a6fdbbc
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2021-12-12 00:04:21 +0100

    Add project files.

---
+++ Day1Star1/Main.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::ifstream inputFile("Input.txt");
+	if (inputFile.is_open())
+	{
+		int i{};
+		int previous{};
+		int current{};
+		inputFile >> previous;
+		while (!inputFile.eof())
+		{
+			inputFile >> current;
+			if (current > previous)
+				i++;
+			previous = current;
+		}
+		std::cout << i;
+		inputFile.close();
+	}
+}
